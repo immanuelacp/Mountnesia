47054919b9bda53e98a28edf991c9c40ccdf3b95
Immanuela Christiantari <immanuelacip@gmail.com>
2020-08-31 09:20:48 +0700

    finish

---
+++ js/index.js
@@ -0,0 +1,10 @@
+$('.cta a').click(function() {
+	$('html,body').animate({
+		scrollTop: $('#tours').offset().top
+	}, 500);
+	return false;
+});
+
+$('#tours li').on('click', function() {
+	$('#location').val($('img', this).attr('alt'));
+});
